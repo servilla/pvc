df4466e3ff01bd7c8b4e7eb2cf1d014dd5da70fe
Mark Servilla <mark.servilla@gmail.com>
2026-01-24 21:12:27 -0700

    Summary: Add environment inspection stubs.

---
+++ src/pvc/inspect_conda.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Summary: Inspect a Conda environment file for a package and its version.
+
+Module:
+    inspect_conda
+
+Author:
+    servilla
+
+Created:
+    2026-01-24
+"""
---
+++ src/pvc/inspect_pixi.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Summary: Inspect a Pixi lock file for a package and its version.
+
+Module:
+    inspect_pixi
+
+Author:
+    servilla
+
+Created:
+    2026-01-24
+"""
